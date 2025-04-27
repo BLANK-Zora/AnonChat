280a9c5b0d21c981142a77d513eb9c86c3090eae
BLANK-Zora <param.saxena1717@gmail.com>
2025-04-27 11:54:52 +0530

    added delete as handler

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,4 +4,4 @@ import NextAuth from "next-auth";
 import { authOptions } from "./options";
 
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST, handler as DELETE };
