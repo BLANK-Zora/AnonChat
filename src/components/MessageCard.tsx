7d75ca674984725835af28f1f836b672c5ab5485
BLANK-Zora <param.saxena1717@gmail.com>
2025-04-27 16:13:03 +0530

    back to DELETE

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -33,7 +33,7 @@ type MessageCardProps = {
 
 const MessageCard = ({ message, onMessageDelete }: MessageCardProps) => {
   const handleDeleteConfirm = async () => {
-    const response = await axios.post<ApiResponse>(
+    const response = await axios.delete<ApiResponse>(
       `/api/delete-message/${message._id}`
     );
 
