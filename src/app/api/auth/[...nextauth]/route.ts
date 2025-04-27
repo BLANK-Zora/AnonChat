8d7299d6c97716d792e989d9f300c57466c05902
BLANK-Zora <param.saxena1717@gmail.com>
2025-04-27 12:07:57 +0530

    removed delete from auth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,4 +4,4 @@ import NextAuth from "next-auth";
 import { authOptions } from "./options";
 
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST, handler as DELETE };
+export { handler as GET, handler as POST };
