0a74b5bc6b7c40dec7cb9aec556fac5fd96ce07d
BLANK-Zora <param.saxena1717@gmail.com>
2025-04-28 21:27:26 +0530

    removed useless req

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -58,10 +58,8 @@ export async function POST(request: Request) {
   }
 }
 
-export async function GET(request: Request) {
+export async function GET() {
   await dbConnect();
-  const req = await request.json();
-  console.log(req._id);
   const session = await getServerSession(authOptions);
   const user: User = session?.user;
   try {
